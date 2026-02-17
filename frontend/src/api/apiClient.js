61079961c3793b71232a2ac410cbf181f7543864
izzabakshi1400 <izzabakshi@gmail.com>
2026-02-17 14:38:25 +0200

    Update apiClient.js

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5293",
+  baseURL: "https://songs-application-bck.onrender.com",
 });
 
 api.interceptors.request.use((config) => {
